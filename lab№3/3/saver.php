b6f915c15186d0c1ea05b4e0432dca976538f462
kamilya <120671774+chmmmle@users.noreply.github.com>
2024-03-29 16:28:06 +0200

    Update saver.php

--- lab№3/3/saver.php
+++ lab№3/3/saver.php
@@ -1,1 +1,24 @@
+<?php
+
+function ToHome()
+{
+    header("Location: index.php");
+    exit();
+}
+
+if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+    ToHome();
+}
+
+$email = $_POST['email'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+$category_folder = "./categories/$category";
+mkdir($category_folder, 0777, true);
+$file_name = "$category_folder/$title.txt";
+$content = "$email\n$title\n$description";
+file_put_contents($file_name, $content);
+ToHome();
+?>
 
