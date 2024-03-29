b16a2b1be81a6269448aed73bb9b44e42cae7bf1
kamilya <120671774+chmmmle@users.noreply.github.com>
2024-03-29 16:17:38 +0200

    Update index.php

--- lab№3/1/index.php
+++ lab№3/1/index.php
@@ -1,1 +1,24 @@
 
+<?php
+// ЗАДАЧА 1.1
+
+$input_str = 'amoba athea abba ayma artya' ;
+$pattern = '/a[a-z][a-z]a/ui';
+preg_match_all($pattern, $input_str, $matches);
+foreach($matches[0] as $match)
+{
+    echo $match, "\n";
+}
+
+
+// ЗАДАЧА 1.2
+function cube($matches) {
+    return pow($matches[0], 3);
+}
+
+$input_str = 'a1b2c3';
+$result = preg_replace_callback('/[0-9]/u', 'cube', $input_str);
+
+echo $result;
+?>
+
