1c6e229ff64373a90470891a7bd04271af56d4b2
kamilya <120671774+chmmmle@users.noreply.github.com>
2024-03-29 16:58:04 +0200

    Update index.php

--- lab№3/2/index.php
+++ lab№3/2/index.php
@@ -3,31 +3,35 @@ session_start();
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $surname = $_POST["surname"];
+if ("POST" === $_SERVER["REQUEST_METHOD"]) {
     $name = $_POST["name"];
     $age = $_POST["age"];
+    $salary = $_POST["salary"];
+    $number = $_POST["number"];
 
     $_SESSION["surname"] = $surname;
     $_SESSION["name"] = $name;
     $_SESSION["age"] = $age;
+    $userData = [
+        'name' => $name,
+        'age' => $age,
+        'salary' => $salary,
+        'number' => $number
+    ];
+
+    $_SESSION['userData'] = $userData;
 
     header("Location: help.php");
     exit();
 }
 ?>
+
 <!DOCTYPE html>
-<html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Область для текста</title>
-    <title>Form</title>
+
 </head>
 <body>
-    <form method="post" action="help.php">
-        <textarea name="text" rows="6"></textarea>
-        <input type="submit" value="count">
-    </form>
 <form method="post">
     <label for="surname">Фамилия:</label>
     <input type="text" id="surname" name="surname" required><br><br>
@@ -37,6 +41,19 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <input type="number" id="age" name="age" required><br><br>
 
     <input type="submit" value="Отправить">
+    <label for="name">Name:</label><br>
+    <input type="text" id="name" name="name"><br>
+
+    <label for="age">Age:</label><br>
+    <input type="text" id="age" name="age"><br>
+
+    <label for="salary">Salary:</label><br>
+    <input type="text" id="salary" name="salary"><br>
+
+    <label for="number">Number:</label><br>
+    <input type="text" id="number" name="number"><br>
+
+    <input type="submit" value="Submit">
 </form>
 </body>
 </html>
