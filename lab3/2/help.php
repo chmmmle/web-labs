f3c8ed976e3269889efdf91cc094f342715bc46f
kamilya <120671774+chmmmle@users.noreply.github.com>
2024-03-29 10:25:34 +0200

    Update help.php

--- lab3/2/help.php
+++ lab3/2/help.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
 
-if (isset($_SESSION["surname"], $_SESSION["name"], $_SESSION["age"])) {
-
-    echo "Фамилия: " . $_SESSION["surname"] . "<br>";
-    echo "Имя: " . $_SESSION["name"] . "<br>";
-    echo "Возраст: " . $_SESSION["age"] . "<br>";
-} else {
-    echo "Данные не найдены в сессии.";
+if(isset($_SESSION['userData'])) {
+    echo '<ul>';
+    foreach ($_SESSION['userData'] as $key => $value) {
+        echo "<li>$key: $value</li>";
+    }
+    echo '</ul>';
 }
+else echo "Не найдено!";
 ?>
