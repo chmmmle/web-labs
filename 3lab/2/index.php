2c8976987401436c6908c4d96a9f41b7cd696f38
kamilya <120671774+chmmmle@users.noreply.github.com>
2024-03-30 16:16:51 +0200

    Update index.php and solve task 2a

--- 3lab/2/index.php
+++ 3lab/2/index.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Область для текста</title>
+</head>
+<body>
+    <form method="post" action="help.php">
+        <textarea name="text" rows="6"></textarea>
+        <input type="submit" value="count">
+    </form>
+</body>
+</html>
